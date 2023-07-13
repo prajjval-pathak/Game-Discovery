dcfbc9d79d6ffd7033c6c148190e9a949873080e
Prajjval Pathak <prajjvalpathak@gmail.com>
2023-07-13 20:34:53 +0530

    Initial Nav bar and Project Layout

---
+++ src/Components/NavBar.tsx
@@ -0,0 +1,16 @@
+import { HStack, Image, Text } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
+import React from "react";
+
+export const NavBar = () => {
+  return (
+    <HStack>
+      <Image
+        src={logo}
+        boxSize="60px
+      "
+      ></Image>
+      <Text>NavBar</Text>
+    </HStack>
+  );
+};
